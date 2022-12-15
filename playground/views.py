9e56598c90f88e8e7b63ad584d197862274c2e98
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2022-12-15 11:15:55 +0800

    using template
    created a template folder and added a hello.html which then renders from views.py with a value being passed

--- playground/views.py
+++ playground/views.py
@@ -11,5 +11,4 @@ def say_hello(request):
     # Pull data from db
     # Transform
     # Send email
-
-    return HttpResponse('Hello World')
+    return render(request, 'hello.html', {'name': 'Harvir'})
